4f18a19c1435bccded945626e5f104a040f86b05
anany mishra <progameranany@gmail.com>
2025-06-27 19:26:50 +0530

    \Fix punctuation and naming conventions for all files and directories
    - Renamed directories with proper capitalization and spacing
    - Updated Python files to use snake_case naming convention
    - Fixed spelling errors (genrator -> generator, vedio -> video)
    - Removed duplicate file extensions
    - Updated band name generator welcome message to be more professional
    - Clarified abbreviations (fb -> facebook, installer -> downloader)"

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,4 +1,4 @@
-print('hello hoe to my fking band name next gen genrator')
+print('Welcome to my next gen band name generator')
 city_name = input("what city you grew up in?\n")
 pet_name = input("what was your pet name\n")
 print(city_name+" "+pet_name)
